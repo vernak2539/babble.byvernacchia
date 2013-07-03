d25df13ba8eda055ccc1f724ed9da6b706b259b4
Alex Vernacchia <avernacchia@exacttarget.com>
2013-07-03 09:43:20 -0400

    bower and grunt task setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,52 +1,47 @@
 /*global module:false*/
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    // Task configuration.
-    jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        unused: true,
-        boss: true,
-        eqnull: true,
-        globals: {}
-      },
-      gruntfile: {
-        src: 'Gruntfile.js'
-      },
-      lib_test: {
-        src: ['lib/**/*.js', 'test/**/*.js']
-      }
-    },
-    nodeunit: {
-      files: ['test/**/*_test.js']
-    },
-    watch: {
-      gruntfile: {
-        files: '<%= jshint.gruntfile.src %>',
-        tasks: ['jshint:gruntfile']
-      },
-      lib_test: {
-        files: '<%= jshint.lib_test.src %>',
-        tasks: ['jshint:lib_test', 'nodeunit']
-      }
-    }
-  });
+	// load all grunt tasks
+	require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+	// Project configuration.
+	grunt.initConfig({
+		recess: {
+			compile: {
+				src: ['./less/styles.less'],
+				dest: './css/styles.css',
+				options: {
+					compile: true
+				}
+			},
+			compress: {
+				src: ['./less/styles.less'],
+				dest: './css/styles.css',
+				options: {
+					compress: true
+				}
+			},
+			bootstrap: {
+				src: [ './components/bootstrap/less/bootstrap.less '],
+				dest: './css/bootstrap.css',
+				options: {
+					compress: true
+				}
 
-  // Default task.
-  grunt.registerTask('default', ['jshint', 'nodeunit']);
+			}
+		},
+		watch: {
+			less: {
+				files: [ './less/*.less' ],
+				tasks: [ 'recess:compile' ]
+			}
+		}
+	});
+
+	// Default task.
+	grunt.registerTask('default', [ 'recess:bootstrap', 'recess:compress' ]);
+
+	// Dev watch task
+	grunt.registerTask('dev', [ 'watch:less' ] );
 
 };
